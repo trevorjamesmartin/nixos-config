6416e573e5ea5d0d9b4fe2d4718060703d78c7dc
Trevor Martin <trevor.j.martin@gmail.com>
2024-06-30 23:46:56 -0400

    fix cursor theme

--- nixos/modules/home-manager/theme.nix
+++ nixos/modules/home-manager/theme.nix
@@ -5,10 +5,8 @@ let
   catppuccin_flavor = "frappe";
   catppuccin_accent = "blue";
   catppuccin_kvantum_theme = "Catppuccin-Frappe-Blue";
-  
   # todo: check how we build these names
   catppuccin_name = "catppuccin-${catppuccin_flavor}-${catppuccin_accent}-standard+default";
-  catppuccin_cursor_name = "catppuccin-${catppuccin_flavor}-${catppuccin_accent}-cursors";
 in
 {
   home.packages = with pkgs; [
@@ -18,6 +16,15 @@ in
     catppuccin-cursors
     tmuxPlugins.catppuccin
   ];
+
+  
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.catppuccin-cursors.frappeBlue;
+    name = "catppuccin-${catppuccin_flavor}-${catppuccin_accent}-cursors";
+    size = 64;
+  };
+
 
   # general settings
   catppuccin = {
@@ -94,9 +101,8 @@ in
     # I'm setting these environment variables for hyprcursor to use
     XCURSOR_THEME = "catppuccin-${catppuccin_flavor}-${catppuccin_accent}-cursors";
     XCURSOR_SIZE  = 64;
+    HYPRCURSOR_SIZE = 64; 
     HYPRCURSOR_THEME = "catppuccin-${catppuccin_flavor}-${catppuccin_accent}-cursors";
-    #HYPRCURSOR_SIZE = 64;
-    
     # pretty 
     QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
     QT_AUTO_SCREEN_SCALE_FACTOR = 1;
