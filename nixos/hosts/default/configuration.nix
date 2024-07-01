4d6e385f7cde2c02bbbfafd2dda675df709e597c
Trevor Martin <trevor.j.martin@gmail.com>
2024-06-30 23:45:38 -0400

    xdg

--- nixos/hosts/default/configuration.nix
+++ nixos/hosts/default/configuration.nix
@@ -3,17 +3,13 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, lib, inputs, ... }:
-let
-  # TODO : system theme
-  catppuccin_name = "catppuccin-frappe-blue-standard+default";
-  catppuccin-gtk = pkgs.catppuccin-gtk;
-  catppuccin_cursor_name = "catppuccin-frappe-blue-cursors";
-in
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./xdg.nix
       ../../cachix.nix
+
       ../../modules/nixos/gdm-theme.nix
     ];
   
@@ -122,7 +118,9 @@ in
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    #package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+
   };
 
   programs.hyprlock.enable = true;
@@ -241,12 +239,6 @@ in
       };
     })
 
-
-#    (self: super: {
-#      hyprland = super.hyprland.override {
-#        plugins = [ self.hyprlandPlugins.hyprexpo self.hyprlandPlugins.hyprbars ];
-#      };
-#    })
   ];
 
   # List packages installed in system profile. To search, run:
@@ -280,7 +272,10 @@ in
     catppuccin
     catppuccin-gtk
     catppuccin-qt5ct
-    #catppuccin-cursors
+    
+    catppuccin-cursors.frappeBlue
+    catppuccin-cursors.mochaMauve
+
 #   # Add global theme
     #paper-icon-theme
     papirus-icon-theme
@@ -421,41 +416,6 @@ in
   services.gvfs.enable = true; # mount, trash and other functionality
   services.tumbler.enable = true; # thumbnail support
  
-  environment.etc = {
-    "xdg/Kvantum/kvantum.kvconfig".source = (pkgs.formats.ini {}).generate "kvantum.kvconfig" {
-      General.theme = "Catppuccin-Frappe-Blue";
-    };
-    
-    "xdg/gtk-4.0".source = "${catppuccin-gtk}/share/themes/${catppuccin_name}/gtk-4.0";
-
-    #"xdg/gtk-3.0".source = "${catppuccin-gtk}/share/themes/${catppuccin_name}/gtk-3.0";
-    
-    "xdg/gtk-3.0/settings.ini" = {
-      text = ''
-      [Settings]
-      gtk-icon-theme-name=Papirus
-      gtk-theme-name=${catppuccin_name}
-      '';
-      mode = "444";
-    };
-
-    # qt 4/5 global theme
-    "xdg/Trolltech.conf" = {
-      text = ''
-      [Qt]
-I     style=Kvantum
-      '';
-      mode = "444";
-    };
-
-    # TODO: system theme 
-
-    #"xdg/gtk-4.0/gtk-dark.css".source = "${catppuccin-gtk}/share/themes/${catppuccin_name}/gtk-4.0/gtk-dark.css";
-
-    #"xdg/gtk-4.0/assets".source = "${catppuccin-gtk}/share/themes/${catppuccin_name}/gtk-4.0/assets";
-    #"xdg/gtk-2.0".source =  ./gtk-2.0;
-    #"xdg/gtk-3.0".source =  ./gtk-3.0;
-  };
 
   environment.sessionVariables = {
     XDG_RUNTIME_DIR = "/run/user/$(id -u)";
@@ -492,15 +452,6 @@ I     style=Kvantum
   ];
   
   #services.flatpak.enable = true;
-   
-  xdg = {
-    portal = {
-      enable = true;
-      extraPortals = with pkgs; [
-#       xdg-desktop-portal-gtk
-      ];
-    };
-  }; 
 
   programs.steam = {
     enable = true;
