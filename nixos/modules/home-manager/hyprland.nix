296ac57e014c27fb5e77c4c5a100e0c16ff00051
Trevor Martin <trevor.j.martin@gmail.com>
2024-07-02 21:21:19 -0400

    hyprlock at start

--- nixos/modules/home-manager/hyprland.nix
+++ nixos/modules/home-manager/hyprland.nix
@@ -22,7 +22,7 @@
     settings = {
       
       monitor = [
-        "eDP-1,1920x1080@60,0x0,1.2"
+        "eDP-1,1920x1080@60,0x0,1"
         # position desktop monitors (when plugged in)
         "HDMI-A-1,2560x1440@60,0x0,1"
         "DP-2,2560x1440@60,2560x0,1"
@@ -52,6 +52,7 @@
         "/etc/nixos/dotfiles/hypr/scripts/start.sh"
         "hypridle"
         "nwg-dock-hyprland -d -f -w 20"
+        "pidof hyprlock || hyprlock"
       ];
 
       exec = [
