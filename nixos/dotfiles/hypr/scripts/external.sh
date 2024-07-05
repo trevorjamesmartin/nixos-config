b63053bd0ef8ef2d0ca42b633f4de097034e7490
Trevor Martin <trevor.j.martin@gmail.com>
2024-07-05 19:48:39 -0400

    updated

--- nixos/dotfiles/hypr/scripts/external.sh
+++ nixos/dotfiles/hypr/scripts/external.sh
@@ -1,11 +1,2 @@
-# if we have more than 1 display available
-if [[ $(wlr-randr --json |jq length) -gt 1 ]] then
-    # locate the primary display
-    primary=$(wlr-randr --json |jq -r '.[0]| .name');
-    # turn it off
-    wlr-randr --output $primary --off;
-    echo "internal display: OFF"; else
-
-    echo "single display, no changes were made.";
-fi
+if [[ $(wlr-randr --json |jq length) -gt 1 ]] then wlr-randr --output "eDP-1" --off; else wlr-randr --output "eDP-1" --on; fi
 
--- nixos/dotfiles/hypr/scripts/start-waybar.sh
+++ nixos/dotfiles/hypr/scripts/start-waybar.sh
@@ -1,7 +1,4 @@
 # make sure we're only running 1 waybar
-ps -ea |grep waybar|awk '{print $1}' | xargs kill -9 --
-
-#waybar -c ~/dotfiles/waybar/config.jsonc -s ~/dotfiles/waybar/style.css
+pidof waybar | xargs kill -9 --
 waybar -c /etc/nixos/dotfiles/waybar/config.json -s /etc/nixos/dotfiles/waybar/style.css
-
 
