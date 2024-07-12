cb6ef3fd4ee9b623879205970dd424312fd14f4b
Trevor Martin <trevor.j.martin@gmail.com>
2024-07-12 00:24:19 -0400

    declared in theme.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -63,30 +63,6 @@
     ];
   };
 
-  programs.tmux = {
-    enable = true;
-    catppuccin.enable = true;
-    aggressiveResize = true;
-    baseIndex = 1;
-    clock24 = true;
-    sensibleOnTop = true;
-    # This should either be screen-256color or tmux-256color where it exists
-    terminal = "tmux-256color";
-
-    extraConfig = ''
-      set -g status on
-      set -g mouse on
-
-      # Where this shows 'foot' - the value should be whatever $TERM is outside tmux
-      set-option -ga terminal-overrides ",foot:Tc"
-
-      # Catppuccin options
-      set -g @catppuccin_host 'on'
-      set -g @catppuccin_window_tabs_enabled 'on'
-    '';
-  };
-
-
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
