9c40ff517cec9c353e55e8024ca17f37dfc4af50
Trevor Martin <trevor.j.martin@gmail.com>
2024-08-26 18:41:19 -0400

    updated

--- nixos/modules/home-manager/user-scripts/default.nix
+++ nixos/modules/home-manager/user-scripts/default.nix
@@ -26,26 +26,31 @@ in {
 
       (pkgs.writeShellScriptBin "${config.home.username}-update-flakes" ''
         hyprctl dispatch tagwindow +nix
+        echo "[system flake update]"
         sudo nix flake update /etc/nixos
+        echo "[home-manager flake update]"
         nix flake update ${hm_hq}
       '')
     
       (pkgs.writeShellScriptBin "${config.home.username}-local-switch" ''
         hyprctl dispatch tagwindow +nix
+        echo "[home-manager switch]"
         home-manager -f ${hm_hq}/home.nix switch
         hyprctl dispatch tagwindow -- -nix
       '')
 
       (pkgs.writeShellScriptBin "${config.home.username}-system-switch" ''
-        echo "Hello, ${config.home.username}! (ready to run nixos-rebuild switch...)"
         hyprctl dispatch tagwindow +nix
+        echo "[nixos-rebuild switch]"
         sudo nixos-rebuild switch --flake /etc/nixos#default --show-trace
-        #hyprctl dispatch tagwindow -- -nix
+        echo "[home-manager switch]"
+        home-manager -f ${hm_hq}/home.nix switch
+        hyprctl dispatch tagwindow -- -nix
       '')
 
       (pkgs.writeShellScriptBin "${config.home.username}-collect-garbage" ''
         hyprctl dispatch tagwindow +nix
-        echo "Hello, ${config.home.username}! (ready to collect the garbage)"
+        echo "[garbage collection]"
         sudo nix-collect-garbage -d
         nix-collect-garbage -d
         hyprctl dispatch tagwindow -- -nix
@@ -53,7 +58,7 @@ in {
 
       (pkgs.writeShellScriptBin "${config.home.username}-optimize" ''
         hyprctl dispatch tagwindow +nix
-        echo "Hello, ${config.home.username}! (ready to optimize the Nix store)"
+        echo "[optimize nix store]"
         sudo nix-store --optimize -vvv
         nix-store --optimize -vvv
         hyprctl dispatch tagwindow -- -nix
