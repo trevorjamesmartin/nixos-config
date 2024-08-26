9c693fdf7d6b9a2bf304c989d539d6124cffdc42
Trevor Martin <trevor.j.martin@gmail.com>
2024-08-26 19:06:33 -0400

    cleanup

--- nixos/hosts/default/home-manager/flake.nix
+++ nixos/hosts/default/home-manager/flake.nix
@@ -2,22 +2,17 @@
   description = "Home Manager configuration of tm";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     catppuccin.url = "github:catppuccin/nix";
-    
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-    
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
     };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
 
   outputs = inputs @ { 
@@ -34,17 +29,14 @@
     in {
       homeConfigurations."tm" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        
+        # Optionally use extraSpecialArgs to pass through arguments to home.nix       
         extraSpecialArgs = { inherit inputs; };
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [ 
-          ./home.nix
           catppuccin.homeManagerModules.catppuccin
+          ./home.nix
         ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
       };
     };
 }
