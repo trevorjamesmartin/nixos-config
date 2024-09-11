fffefb12cccf62d857a473f73d80983183ab1660
Trevor Martin <trevor.j.martin@gmail.com>
2024-09-11 19:32:41 -0400

    reduce clutter

--- nixos/modules/home-manager/user-scripts/default.nix
+++ nixos/modules/home-manager/user-scripts/default.nix
@@ -1,7 +1,7 @@
-{ hm_hq ? "/etc/nixos/hosts/default/home-manager", hm_hostname ? "default", ... }:
-{ lib, config, pkgs, ... }:
+{ hostName ? "default", ... }:{ lib, osConfig, config, pkgs, ... }:
 with lib;
 let 
+  hm_hq = "/etc/nixos/hosts/${hostName}/home-manager";
   cfg = config.yoshizl.user-scripts;
 in {
   options.yoshizl.user-scripts.enable = mkEnableOption "enable user-scripts";
@@ -9,20 +9,6 @@ in {
   config = mkIf cfg.enable {
 
     home.packages = [
-
-    # (pkgs.writeShellScriptBin "${config.home.username}-local-update" ''
-    #   echo "Hello, ${config.home.username}! (ready to update & run home-manager switch...)"
-    #   hyprctl dispatch tagwindow +nix
-    #   echo "[enter home mangager]"
-    #   pushd ~/.config/home-manager
-    #   echo "[flake update]"
-    #   nix flake update .
-    #   echo "[home-manger switch]"
-    #   home-manager switch --impure --show-trace
-    #   echo "[exit home-manager]"
-    #   popd
-    #   hyprctl dispatch tagwindow -- -nix
-    # '')
 
       (pkgs.writeShellScriptBin "${config.home.username}-update-flakes" ''
         hyprctl dispatch tagwindow +nix
@@ -42,7 +28,7 @@ in {
       (pkgs.writeShellScriptBin "${config.home.username}-system-switch" ''
         hyprctl dispatch tagwindow +nix
         echo "[nixos-rebuild switch]"
-        sudo nixos-rebuild switch --flake /etc/nixos#${hm_hostname} --show-trace
+        sudo nixos-rebuild switch --flake /etc/nixos#${hostName} --show-trace
         echo "[home-manager switch]"
         home-manager -f ${hm_hq}/home.nix switch
         hyprctl dispatch tagwindow -- -nix
