f62cf1c647cf5f4fe7a2cf238d2d007f9b34e0dc
Trevor Martin <trevor.j.martin@gmail.com>
2024-09-11 20:05:27 -0400

    lock nixpkgs

--- nixos/hosts/desktop/home-manager/flake.nix
+++ nixos/hosts/desktop/home-manager/flake.nix
@@ -2,8 +2,9 @@
   description = "Home Manager configuration of tm";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     #nixpkgs.url = "github:NixOS/nixpkgs/b79ce4c43f9117b2912e7dbc68ccae4539259dda";
+    nixpkgs.url = "github:NixOS/nixpkgs/574d1eac1c200690e27b8eb4e24887f8df7ac27c"; # (2024-09-06)
     #nixpkgs.url = "github:nixos/nixpkgs/24.05";
     catppuccin.url = "github:catppuccin/nix";
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
