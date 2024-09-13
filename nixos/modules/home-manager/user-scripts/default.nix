97ac3cdca9129a3ae8c4beae157ebe5ace35f167
Trevor Martin <trevor.j.martin@gmail.com>
2024-09-13 15:05:07 -0400

    updated

--- nixos/modules/home-manager/user-scripts/default.nix
+++ nixos/modules/home-manager/user-scripts/default.nix
@@ -1,7 +1,6 @@
-{ hostName ? "default", ... }:{ lib, osConfig, config, pkgs, ... }:
+{ lib, osConfig, config, pkgs, ... }:
 with lib;
 let 
-  hm_hq = "/etc/nixos/hosts/${hostName}/home-manager";
   cfg = config.yoshizl.user-scripts;
 in {
   options.yoshizl.user-scripts.enable = mkEnableOption "enable user-scripts";
@@ -9,30 +8,6 @@ in {
   config = mkIf cfg.enable {
 
     home.packages = [
-
-      (pkgs.writeShellScriptBin "${config.home.username}-update-flakes" ''
-        hyprctl dispatch tagwindow +nix
-        echo "[system flake update]"
-        sudo nix flake update /etc/nixos
-        echo "[home-manager flake update]"
-        nix flake update ${hm_hq}
-      '')
-    
-      (pkgs.writeShellScriptBin "${config.home.username}-local-switch" ''
-        hyprctl dispatch tagwindow +nix
-        echo "[home-manager switch]"
-        home-manager -f ${hm_hq}/home.nix switch
-        hyprctl dispatch tagwindow -- -nix
-      '')
-
-      (pkgs.writeShellScriptBin "${config.home.username}-system-switch" ''
-        hyprctl dispatch tagwindow +nix
-        echo "[nixos-rebuild switch]"
-        sudo nixos-rebuild switch --flake /etc/nixos#${hostName} --show-trace
-        echo "[home-manager switch]"
-        home-manager -f ${hm_hq}/home.nix switch
-        hyprctl dispatch tagwindow -- -nix
-      '')
 
       (pkgs.writeShellScriptBin "${config.home.username}-collect-garbage" ''
         hyprctl dispatch tagwindow +nix
