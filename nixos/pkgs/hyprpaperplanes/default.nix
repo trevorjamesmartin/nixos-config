4bf562a6d6fd53dcbefb622e5ae6f3fb7fe60779
Trevor Martin <trevor.j.martin@gmail.com>
2024-09-26 11:39:15 -0400

    moved hyprpaperplanes to it's own repo

--- nixos/pkgs/hyprpaperplanes/default.nix
+++ nixos/pkgs/hyprpaperplanes/default.nix
@@ -1,10 +1,15 @@
-{ buildGoModule, lib, ... }:
+{ buildGoModule, lib, fetchFromGitHub, ... }:
 
 buildGoModule rec {
   pname = "hyprpaperplanes";
-  version = "0.2";
+  version = "0.3";
 
-  src = ./src;
+  src = fetchFromGitHub {
+    owner = "trevorjamesmartin";
+    repo = "hyprpaperplanes";
+    rev = "8938fa8f4344b6149d8d65aa4ceca4b06d67d489";
+    hash = "sha256-F9wTaHTYW/L+zYWBOgWRelk6I+y6zX4Y6MXlnaA7kj8=";
+  };
   vendorHash = null;
 
   meta = with lib; {
--- nixos/pkgs/hyprpaperplanes/src/go.mod
+++
@@ -1,3 +0,0 @@
-module hyprPaperPlanes
-
-go 1.22
