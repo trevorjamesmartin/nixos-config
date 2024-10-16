a700f1d5264bee58da678c5934c120062c64e005
Trevor Martin <trevor.j.martin@gmail.com>
2024-10-16 11:23:03 -0400

    updated to build from flake

--- nixos/modules/home-manager/hyprpaper/default.nix
+++ nixos/modules/home-manager/hyprpaper/default.nix
@@ -1,22 +1,15 @@
-{ lib, config, pkgs, ...}:
+{ inputs, lib, config, pkgs, ...}:
 with lib;
 let cfg = config.yoshizl.hyprpaper;
 in {
+
   options.yoshizl.hyprpaper.enable = mkEnableOption "enable hyprpaper";
-
+  
   config = mkIf cfg.enable {
-
-    nixpkgs.overlays = [
-       (self: super: {
-        hypsi = pkgs.callPackage ./hypsi.nix {};
-       })
-    ];
-
     home.packages = with pkgs; [
       hyprpaper
-      hypsi
+      inputs.hypsi.defaultPackage.x86_64-linux 
     ];
-    
   };
 
 }
